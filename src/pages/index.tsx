cbf6233744a2c951099ea7fd93af46d73dc9b4bb
igorheije <heije.tomori2014@outlook.com>
2021-02-16 10:08:42 -0300

    config projet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,20 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 
-export default function Home() {
+type Props = {
+  title: string
+}
+
+export default function Home({ title = 'React Avançado!' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>{title}</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
